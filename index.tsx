5f660745468c1dc259b55306553b1cf560073e65
DangPHHE172554 <dangphhe172554@fpt.edu.vn>
2025-12-07 13:58:32 +0700

    fix: implement BrowserRouter with correct basename

--- index.tsx
+++ index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+
+const BASE_PATH = "/ho-chi-minh-mot-cuoc-doi-mot-dan-toc/";
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
